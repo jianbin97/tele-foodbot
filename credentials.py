d0efafbfe5b72507e9509005b94e539498f43a82
jblol <60912341+jianbin97@users.noreply.github.com>
2021-07-08 23:52:05 +0800

    fix requirements.txt

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 bot_token = '1808167854:AAFg5o6EWUg9ikyU57Eo4SKLXiHb58aH4y4'
 bot_user_name = 'Foooodie_bot'
-URL = 'https://git.heroku.com/tele-foodie-bot.git'
+URL = 'https://tele-foodie-bot.herokuapp.com/'
